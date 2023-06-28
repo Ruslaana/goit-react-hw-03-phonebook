0ebc2cd6ff4442f1c7e123382883c999d8015f10
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-06-28 14:47:21 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,19 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = data => {
     const { contacts } = this.state;
     const newContact = {
